de8cf68cc68eba5eebb49134eeae89de03a633a6
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-01-24 17:15:33 -0800

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -7,12 +7,12 @@ class Umbrella {
        }
        this.body = Bodies.circle(x,y,this.radius/2, umbrella_options);
        this.radius = radius;
-       this.animation = loadAnimation("images/Walking Frame/walking_1.png","images/Walking Frame/walking_2.png", "images/Walking Frame/walking_3.png", "images/Walking Frame/walking_4.png", "images/Walking Frame/walking_5.png", "images/Walking Frame/walking_6.png", "images/Walking Frame/walking_7.png", "images/Walking Frame/walking_8.png");
+       this.image = loadImage("images/Walking Frame/walking_1.png");
        World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
         imageMode(CENTER);
-        image(this.animation, position.x,position.y,this.radius, this.radius);
+        image(this.image, position.x,position.y,this.radius, this.radius);
     }
 }
