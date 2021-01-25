8cad6449cf24a2ed640d074977fd025acd36d05b
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-01-24 17:15:56 -0800

    Update drops.js

--- drops.js
+++ drops.js
@@ -7,12 +7,11 @@ class Drops {
         }
         this.body = Matter.Bodies.circle(x,y,this.radius/2, drops_options);
         this.radius = radius;
-        this.animation = loadAnimation("images/thunderbolt/1.png","images/thunderbolt/2.png", "images/thunderbolt/3.png", "images/thunderbolt/4.png");
         World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
-        imageMode(CENTER);
-        image(this.animation, position.x, position.y, this.radius, this.radius);
+        ellipseMode(CENTER);
+        ellipse(position.x, position.y, this.radius, this.radius);
     }
 }
