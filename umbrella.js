0c2e0d2e8f5f35b7a50bf8470c37591ad45d1b47
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-01-27 21:05:04 -0800

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -1,18 +1,18 @@
 class Umbrella {
-    constructor(x,y,radius){
+    constructor(x,y){
        var umbrella_options ={
            isStatic: true,
            density: 1,
            friction: 1
        }
-       this.body = Bodies.circle(x,y,this.radius/2, umbrella_options);
-       this.radius = radius;
+       this.body = Bodies.circle(x,y,50, umbrella_options);
+       this.radius = 50;
        this.image = loadImage("images/Walking Frame/walking_1.png");
        World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
         imageMode(CENTER);
-        image(this.image, position.x,position.y,this.radius, this.radius);
+        image(this.image, position.x-30,position.y+200,200,200);
     }
 }
