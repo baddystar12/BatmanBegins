7e18bb9ca2845981427454d343f41ed9188c63fc
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2021-01-27 21:04:43 -0800

    Update drops.js

--- drops.js
+++ drops.js
@@ -1,17 +1,23 @@
 class Drops {
-    constructor(x,y,radius){
+    constructor(x,y){
         var drops_options ={
             isStatic: false,
             density: 1,
-            friction:1
+            restitution: 0.1
         }
-        this.body = Matter.Bodies.circle(x,y,this.radius/2, drops_options);
-        this.radius = radius;
+        this.body = Matter.Bodies.circle(x,y,5, drops_options);
+        this.radius = 5;
         World.add(world, this.body);
     }
     display(){
         var position = this.body.position;
         ellipseMode(CENTER);
+        fill("turquoise");
         ellipse(position.x, position.y, this.radius, this.radius);
+    }
+    update(){
+        if(this.body.position.y>900){
+            Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)});
+        }
     }
 }
